d4b3a1106cb6cf9a1bb27e3b2a6006b4db53f674
parhan <apriliandi642@gmail.com>
2020-10-19 22:14:07 +0700

    add middleware

---
+++ middleware/validationId.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+module.exports = (req, res, next) => {
+   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(404).send({
+         message: "Invalid ID....",
+         status: res.statusCode,
+      });
+   }
+
+   next();
+};
